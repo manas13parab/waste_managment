067c3794377e32515a61ee998c1485971f48158a
manas13parab <mscids2024.manas@unigoa.ac.in>
2025-10-06 10:56:00 +0530

    Updated models.py file for flask and  restrount.py

--- models.py
+++ models.py
@@ -1,8 +1,16 @@
-from app import db
+from waste_managment.extensions import db
 
 class Restaurant(db.Model):
+    """
+    Represents a restaurant entity with an ID and name.
+
+    Attributes:
+        id (int): Primary key for the restaurant.
+        name (str): Name of the restaurant, cannot be null.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
