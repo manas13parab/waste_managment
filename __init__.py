c627fe984c32c5da256ebdbfefdced25af1ef6a5
manas13parab <mscids2024.manas@unigoa.ac.in>
2025-10-09 12:23:13 +0530

    Flask method final freeze

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""waste_managment package initializer.
+
+This file makes the folder a Python package so you can import using
+`python -m waste_managment.app` and use package-style imports.
+"""
+
+from .app import app
+
+__all__ = ["app"]
