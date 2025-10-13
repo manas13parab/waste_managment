e6609f2658ee4810d6cf8902dbe7b166092ac131
manas13parab <mscids2024.manas@unigoa.ac.in>
2025-10-13 21:56:41 +0530

    added a tracking function which will handle waste type,quantity and restaurant id

--- waste tracking.py
+++ waste tracking.py
@@ -1,2 +1,7 @@
 import json
 from utils import db_waste
+
+def track(handler, post_data):
+    waste_type = post_data.get('waste_type', [''])[0]
+    quantity = float(post_data.get('quantity', ['0'])[0])
+    restaurant_id = post_data.get('restaurant_id', [''])[0]
