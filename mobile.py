1038895a43effdad38576709f8a791d511a431cb
manas13parab <mscids2024.manas@unigoa.ac.in>
2025-11-03 11:00:19 +0530

    mobile.py file

--- mobile.py
+++ mobile.py
@@ -2,7 +2,7 @@ import json
 
 def waste_summary(handler, query):
     summary = {"message": "This is mobile friendly waste summary."}
-    handler.send_response(500)
+    handler.send_response(200)
     handler.send_header('Content-Type', 'application/json')
     handler.end_headers()
     handler.wfile.write(json.dumps(summary).encode())
